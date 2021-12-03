413158afbe6994edb6b3c5b864bf809a58dbe882
chechavalerii <chechavalera@gmail.com>
2021-12-03 17:40:34 +0200

    init

---
+++ src/server/events.js
@@ -0,0 +1,48 @@
+/**
+ * @function
+ * @param  {NodeJS.ErrnoException} error
+ * @returns throw error
+ */
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    switch (error.code) {
+    case 'EACCES':
+        console.error('Port requires elevated privileges');
+        process.exit(1);
+    case 'EADDRINUSE':
+        console.error('Port is already in use');
+        process.exit(1);
+    default:
+        throw error;
+    }
+}
+/**
+ * @function
+ * @inner
+ * @description log port to console
+ */
+function onListening() {
+    const addr = this.address();
+    const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+
+    console.log(`Listening on ${bind}`);
+}
+
+/**
+ * @function
+ * @inner
+ * @param {http.Server} server
+ */
+function bind(server) {
+    server.on('error', (error) => this.onError.bind(server)(error));
+    server.on('listening', this.onListening.bind(server));
+}
+
+module.exports = {
+    onError,
+    onListening,
+    bind,
+};
---
+++ src/server/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const events = require('./events');
+const server = require('./server');
+
+const port = server.get('port');
+
+events.bind(
+    http.createServer(server).listen(port),
+);
