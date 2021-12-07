9840f9bcff0afe48cf08b4ee2d7c7efbd222ecf1
alenworld <inalipa.alen@gmail.com>
2021-12-07 14:32:52 +0200

    Installed dotenv. Created env.js for easy use environments

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-
-const MONGODB_URI = 'mongodb://localhost:27017/';
-const MONGODB_DB_MAIN = 'users_db';
-const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
+const { MONGO_URI } = require('./env');
 
 const connectOptions = {
   useNewUrlParser: true,
---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : process.env.MONGO_URI_LOCAL,
+};
