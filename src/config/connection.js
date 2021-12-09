54177c37a05a8cfa4ccb81185cd2b9ab8054513c
Rostislav Kalyuzhny <rkalyuzhniyy@gmail.com>
2021-12-09 20:07:45 +0200

    Changed connection arguments

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://localhost:27017/';
+const MONGODB_USER = 'rostislav';
+const MONGODB_PASSWORD = 'jqsZA5CXnB77Kzs';
+const MONGODB_CLUSTER = 'cluster1';
 const MONGODB_DB_MAIN = 'users_db';
-const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
+const MONGO_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.n8lff.mongodb.net/${MONGODB_DB_MAIN}?retryWrites=true&w=majority`;
 
 const connectOptions = {
     useNewUrlParser: true,
