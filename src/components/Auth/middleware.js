26bc85b1f4d462951f55bf7bee1cc73f95c4a0a7
Rostislav Kalyuzhny <rkalyuzhniyy@gmail.com>
2021-12-12 15:22:20 +0200

    Changed read way user token

--- src/components/Auth/middleware.js
+++ src/components/Auth/middleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    const token = req.get('x-access-token');
 
     if (!token) {
         return res.status(403).send('A token is required for authentication');
