6f6aeabd0063c612cbfc93dd76b946e32f68e930
alenworld <inalipa.alen@gmail.com>
2022-01-03 23:01:00 +0200

    feat(chat): first working versions of functions: send message and list messages

--- src/assets/js/chat.js
+++ src/assets/js/chat.js
@@ -9,31 +9,56 @@ const nameBlock = document.querySelector('.name');
 const userName = prompt('Enter your name: ');
 nameBlock.innerHTML = `${userName}`;
 
-inputArea.addEventListener('change', (event) => {
-  event.preventDefault();
-  console.log(event.target.value);
-  if (event.target.value) {
-    socket.emit('chat message', {
-      message: event.target.value, name: userName,
-    });
-    event.target.value = '';
-  }
-});
-
 socket.on('chat message', (data) => {
   const item = document.createElement('li');
-  const messageData = item.appendChild(document.createElement('div').setAttribute('class', 'message-data'));
-  const msgDataName = messageData.appendChild(document.createElement('span').setAttribute('class', 'message-data-name'));
-  msgDataName.textContent = `${data.name}`;
 
-  const msgDataTime = messageData.appendChild(document.createElement('span').setAttribute('class', 'message-data-time'));
-  msgDataTime.textContent = `${new Date().toLocaleString('en-US', {
+  const messageData = document.createElement('div');
+  messageData.setAttribute('class', 'message-data');
+
+  const dataName = document.createElement('span');
+  dataName.setAttribute('class', 'message-data-name');
+  const nameValue = document.createTextNode(`${data.name}`);
+
+  // insert name to span
+  dataName.appendChild(nameValue);
+  // insert span elements to div
+  messageData.appendChild(dataName);
+
+  const dataTime = document.createElement('span');
+  dataTime.setAttribute('class', 'message-data-time');
+  const timeValue = document.createTextNode(`${new Date().toLocaleString('en-US', {
     hour: 'numeric',
     minute: 'numeric',
     hour12: true,
-  })}`;
+  })}`);
+  // insert time to span
+  dataTime.appendChild(timeValue);
+  // insert span elements to div
+  messageData.appendChild(dataTime);
 
-  const messageBody = item.appendChild(document.createElement('div').setAttribute('class', 'message my-message'));
-  messageBody.textContent = `${data.message}`;
+  // messageData to item li
+  item.appendChild(messageData);
+
+  // create element for message
+  const messageTag = document.createElement('div');
+  messageTag.setAttribute('class', 'message my-message');
+  const messageValue = document.createTextNode(`${data.message}`);
+  // insert message to element
+  messageTag.appendChild(messageValue);
+
+  // insert messageTag to item li
+  item.appendChild(messageTag);
+  // insert item to chat-history
   messages.appendChild(item);
+});
+
+inputArea.addEventListener('change', (e) => {
+  e.preventDefault();
+  if (e.target.value) {
+    socket.emit('chat message', {
+      message: e.target.value, name: userName,
+    });
+    e.target.value = '';
+  }
+  window.scrollTo(0, document.body.scrollHeight);
 });
