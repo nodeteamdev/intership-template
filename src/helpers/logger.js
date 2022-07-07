f6c68482f1bd72d5cc2fb653f272fc98eba59f00
Alex Oleynik <oleynik.webdev@gmail.com>
2022-07-07 13:54:00 +0300

    feat(logs): created logger helper;

---
+++ src/helpers/logger.js
@@ -0,0 +1,48 @@
+const consoleColorCodes = {
+    reset: '\x1b[0m',
+    bright: '\x1b[1m',
+    fgRed: '\x1b[31m',
+    fgMagenta: '\x1b[35m',
+};
+
+const logLevelToConsoleColorCode = {
+    log: consoleColorCodes.bright,
+    error: consoleColorCodes.fgRed,
+    warn: consoleColorCodes.fgMagenta,
+};
+
+function colorString(string, logLevel) {
+    return (logLevelToConsoleColorCode[logLevel] || consoleColorCodes.reset)
+        + string
+        + consoleColorCodes.reset;
+}
+
+const TAKE_N_FIRST_FROM_STACK = 2;
+function trimError(error) {
+    const errorStackAsArray = error.stack.split('\n');
+    return errorStackAsArray
+        .splice(0, Math.min(TAKE_N_FIRST_FROM_STACK, errorStackAsArray.length))
+        .join('\n');
+}
+
+function logger(dataToLog, context = '', logLevel = 'log') {
+    const timeStr = (new Date()).toJSON();
+
+    let logStr = `[${timeStr}]:`;
+
+    if (context) {
+        logStr += ` {{ ${context} }} `;
+    }
+
+    logStr = colorString(logStr, logLevel);
+
+    if (dataToLog instanceof Error) {
+        logStr += trimError(dataToLog);
+    } else {
+        logStr += dataToLog.toString();
+    }
+
+    console[logLevel](logStr);
+}
+
+module.exports = logger;
