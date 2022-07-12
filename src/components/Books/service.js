1ff145254849a364ae1be5f44d5416d76fae5f1b
agychko <agychko@gmail.com>
2022-07-13 00:48:03 +0300

    funktion count-per-country finished

--- src/components/Books/service.js
+++ src/components/Books/service.js
@@ -2,9 +2,10 @@ const BooksModel = require('./books.model');
 
 function countPerCountry() {
     return BooksModel.aggregate([
-        { $group: { _id: '$code3', value: { $sum: 1 } } },
-        { $project: { code3: { $toUpper: '$_id' }, _id: 0, value: 1 } },
+        { $group: { _id: '$code3', count: { $sum: 1 } } },
+        { $project: { code3: '$_id', count: 1, _id: 0 } },
         { $sort: { code3: 1 } },
+        { $project: { code3: 1, value: '$count' } },
     ]);
 }
 
