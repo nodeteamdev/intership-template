7483d38f38c82970391ec53b48c9a5b0b8a77a89
agychko <agychko@gmail.com>
2022-07-14 09:26:32 +0300

    finish get new books

--- src/components/Books/service.js
+++ src/components/Books/service.js
@@ -3,14 +3,22 @@ const BooksModel = require('./books.model');
 function countPerCountry() {
     return BooksModel.aggregate([
         { $group: { _id: '$code3', count: { $sum: 1 } } },
-        { $project: { code3: '$_id', count: 1, _id: 0 } },
+        { $project: { code3: '$_id', _id: 0, value: '$count' } },
         { $sort: { code3: 1 } },
-        { $project: { code3: 1, value: '$count' } },
     ]);
 }
 
 function getNewBooks() {
-    return BooksModel.aggregate();
+    return BooksModel.aggregate([
+        {
+            $match: {
+                $or: [
+                    { createdAt: { $gte: new Date(new Date() - 7 * 24 * 60 * 60 * 1000) } },
+                    { updatedAt: { $gte: new Date(new Date() - 7 * 24 * 60 * 60 * 1000) } },
+                ],
+            },
+        },
+    ]);
 }
 
 module.exports = {
