4244aac8559566c26e6f8c9b909bb991c3bb0449
Alex Oleynik <oleynik.webdev@gmail.com>
2022-07-16 00:22:44 +0300

    refactor(env)!: mongodb uri moved to .env file;

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const logger = require('../helpers/logger');
 
-const MONGODB_URI = 'mongodb://localhost:27017/';
-const MONGODB_DB_MAIN = 'users_db';
-const MONGO_URI = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
-
 const connectOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 2000,
 };
 
-const mongooseConnection = mongoose.createConnection(MONGO_URI, connectOptions, (error) => {
-    if (error) {
-        logger(error.message, 'mongooseConnection error', 'error');
-        process.exit(1);
-    }
-});
+const mongooseConnection = mongoose.createConnection(
+    process.env.MONGODB_URI,
+    connectOptions,
+    (error) => {
+        if (error) {
+            logger(error.message, 'mongooseConnection error', 'error');
+            process.exit(1);
+        }
+    },
+);
 
 mongooseConnection.on('error', (error) => {
     logger(error, 'mongooseConnection.onError event', 'error');
